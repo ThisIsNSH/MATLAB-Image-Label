2d5b5723661869304b4be1a918b2e541347ba0bc
ThisIsNsh <hadanis.singh@gmail.com>
2020-01-06 16:02:01 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,14 @@
+# MATLAB-Image-Label
+
+### Image Processing
+
+Labelling 4-Way connected components in a BW Image.
+--------
+* I have taken a matrix `a` which in similar to 2-D pixel array of an BW image containing 1 & 0.
+* I have given priority to left pixel over top pixel.
+* For 4-Way labelling I check only the intensity of left & top pixel of a particular pixel.  
+* If the intensity matches I label accordingly. 
+
+<img src="im1.PNG">  
+
+
---
+++ labels.m
@@ -0,0 +1,41 @@
+function a = labels(arr)
+  label = 1;
+  for row = 1:size(arr)(1)
+    for col = 1:size(arr)(2)
+      if arr(row,col)==1
+        if row==1
+          if col==1
+            arr(row,col)=label;
+            label=label+1;
+          elseif arr(row,col-1)==0 
+            arr(row,col)=label;
+            label=label+1;
+          elseif arr(row,col-1)!=0 
+            arr(row,col)=arr(row,col-1);
+          end
+        else
+          if col==1
+            if arr(row-1,col)==0
+              arr(row,col) = label;
+              label=label+1;
+             else
+              arr(row,col) = arr(row-1,col);
+             end
+          else
+            if arr(row,col-1)==0 
+              if arr(row-1,col)==0 
+                arr(row,col)=label;
+                label=label+1;
+              else
+                arr(row,col)=arr(row-1,col);
+              end
+            else 
+              arr(row,col)=arr(row,col-1);
+            end
+          end
+        end
+      endif
+    endfor
+  endfor
+  a = arr;
+endfunction
